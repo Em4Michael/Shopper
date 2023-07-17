51ceda0b24427c45fecff9831d7f79fe21fcbd73
em4michael <aniehegc@gmail.com>
2023-07-17 18:50:15 -0400

    day 11

--- frontend/src/slices/cartSlice.js
+++ frontend/src/slices/cartSlice.js
@@ -2,6 +2,10 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = localStorage.getItem("cart") ? JSON.parse(localStorage.getItem("cart")) : {cartItems: []}
 
+const addDecimals = (num) => {
+    return (math.round(num * 100) / 100).toFixed(2)
+}
+
 const cartSlice = createSlice({
     name: "cart",
     initialState,
@@ -16,8 +20,27 @@ const cartSlice = createSlice({
             } else {
                 state.cartItems = [...state.cartItems, item]
             }
+
+
+            //calculate items price
+            state.itemsPrice = addDecimals(state.cartItems.reduce((acc, item) => acc + item.price * item.qty, 0))
+
+            //calculate items shipping (if over $10 free else free)
+            state.shippingPrice = addDecimals(state.itemsPrice > 100 ? 0 : 10)
+            //calculate items tax (15% tax)
+            state.taxPrice = addDecimals(Number((0.15*state.itemsPrice) toFixed(2)))
+            //calculate items total 
+            state.totalPrice = (
+                Number(state.itemsPrice) +
+                Number(state.shippingPrice) +
+                Number(state.taxPrice) 
+            ).toFixed(2)
+
+            localStorage.setItem('cart', JSON.stringify(state))
         }
     },
 })
+
+export  const { addToCart } = cartSlice.actions 
 
 export default cartSlice.reducer
